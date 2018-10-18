af92beee4e2038e04be81927aa9f4d175ec92bd4
Robert Banick <rbanick@gmail.com>
2018-10-18 22:54:22 +0545

    separating adm1 and adm2 pop_analysis scripts due to unknown glitch in adm2 process for adm1 data. Uploading current versions.

--- ct_classification_analysis/R_CT_Adm1_Charts.r
+++ ct_classification_analysis/R_CT_Adm1_Charts.r
@@ -31,17 +31,15 @@ type <- args[3]
 adm1_input_slim <- subset(adm1_input, select = c("adm_name","adm_code","trav_value","trav_cat","adm_pop","cat_pop","pc_pop")) # create working subset
 adm1_input_slim$trav_cat <- factor(adm1_input_slim$trav_cat, levels = c("0 to 30 minutes","30 minutes to 1 hour","1 to 2 hours","2 to 4 hours","4 to 8 hours","8 to 16 hours","16 to 32 hours","> 32 hours"))  #order trav_cat in the right way
 adm1_input_slim$cat_pop <- prettyNum(round(adm1_input_slim$cat_pop,digits=0),big.mark=",",scientific=FALSE) # make population round with commas
+adm1_input_slim$pc_pop <- adm1_input_slim$pc_pop # make population round with commas
+adm1_input_slim$pc_pop <- round(adm1_input_slim$pc_pop,digits=4) # make population round with commas
 adm1_input_slim$trav_value <- as.factor(adm1_input_slim$trav_value)
 adm1_input_slim[order(adm1_input_slim$adm_code,adm1_input_slim$trav_cat),]
+adm1_order <- adm1_input_slim[order(adm1_input_slim$adm_code,adm1_input_slim$trav_cat),]
 
 ## define color palette variablesadm1_list=list()
 
-LtoM <-colorRampPalette(c('yellow', 'red' ))
-Mid <- "snow3"
-MtoH <-colorRampPalette(c('darkgreen', 'lightgreen'))
-cols <- c("1" = 'darkgreen', "2" = '#2ab72e', "3" = 'lightgreen', "4" = '#f4f007', "5" = '#FECC42', "6" = '#f96a34', "7" = "red")
-
-
+cols <- c("1" = 'darkgreen', "2" = '#2ab72e', "3" = 'lightgreen', "4" = '#e3eb96', "5" = '#f4f007', "6" = '#FECC42', "7" = '#f96a34', "8" = "red")
 ## charts setup
 
 setwd(export_directory) # setwd to exports charts to
@@ -50,9 +48,9 @@ setwd(export_directory) # setwd to exports charts to
 
 adm1_list=list()
 
-for (i in unique(adm1_input_slim$adm_code)) {
+for (i in unique(adm1_order$adm_code)) {
 
-  adm1_filter <- filter(adm1_input_slim, adm_code == i)
+  adm1_filter <- filter(adm1_order, adm_code == i)
 
   plot <- ggplot(adm1_filter, aes(x = trav_cat, y = pc_pop, fill = trav_value)) +
     geom_text(aes(label=paste((pc_pop*100),"%\n",cat_pop," persons",sep="")),hjust=-0.1,size=2.5) +
@@ -65,7 +63,7 @@ for (i in unique(adm1_input_slim$adm_code)) {
     scale_fill_manual(values= cols) +
     coord_flip()
 
-  filename=paste(adm1_filter$adm_name," - ",i," - ",type,".pdf",sep="")
+  filename=paste("./",export_directory,"/","Province ",adm1_filter$adm_name," - ",type,".pdf",sep="")
 
   adm1_list[[i]] = plot
 
@@ -74,11 +72,11 @@ for (i in unique(adm1_input_slim$adm_code)) {
 print("done with plots")
 
 
-## LGU print loop
+## province print loop
 
-for (i in unique(adm1_input_slim$adm_code)) { # Another for loop, this time to save out the bar charts in adm1_list as PDFs
-    adm1_filter <- filter(adm1_input_slim, adm_code == i)
-    filename=paste(adm1_filter$adm_name," - ",i," - ",type,".jpeg",sep="") # Make the file name for each PDF. The paste makes the name a variable of the admin, so each chart is named by LGU and code
+for (i in unique(adm1_order$adm_code)) { # Another for loop, this time to save out the bar charts in adm1_list as PDFs
+    adm1_filter <- filter(adm1_order, adm_code == i)
+    filename=paste("Province ",adm1_filter$adm_name," - ",type,".jpeg",sep="") # Make the file name for each PDF. The paste makes the name a variable of the admin, so each chart is named by province and code
 
     jpeg(filename,width=3.5,height=4,units="in",bg="white",quality=1,res=300,type=c("quartz")) # jpeg basic specifications. Modify the width and height here.
 
--- ct_classification_analysis/R_CT_Adm2_Charts.r
+++ ct_classification_analysis/R_CT_Adm2_Charts.r
@@ -28,18 +28,17 @@ type <- args[3]
 
 ## revise data
 
-lgu_input_slim <- subset(lgu_input, select = c("adm_name","adm_code","trav_value","trav_cat","adm_pop","cat_pop","pc_pop")) # create working subset
+lgu_input_slim <- subset(lgu_input, select = c("adm_name","adm2_code","province","trav_value","trav_cat","adm_pop","cat_pop","pc_pop")) # create working subset
 lgu_input_slim$trav_cat <- factor(lgu_input_slim$trav_cat, levels = c("0 to 30 minutes","30 minutes to 1 hour","1 to 2 hours","2 to 4 hours","4 to 8 hours","8 to 16 hours","16 to 32 hours","> 32 hours"))  #order trav_cat in the right way
 lgu_input_slim$cat_pop <- prettyNum(round(lgu_input_slim$cat_pop,digits=0),big.mark=",",scientific=FALSE) # make population round with commas
+lgu_input_slim$pc_pop <- lgu_input_slim$pc_pop # make population round with commas
+lgu_input_slim$pc_pop <- round(lgu_input_slim$pc_pop,digits=4) # make population round with commas
 lgu_input_slim$trav_value <- as.factor(lgu_input_slim$trav_value)
-lgu_input_slim[order(lgu_input_slim$adm_code,lgu_input_slim$trav_cat),]
+lgu_order <- lgu_input_slim[order(lgu_input_slim$adm2_code,lgu_input_slim$trav_cat),]
 
 ## define color palette variableslgu_list=list()
 
-LtoM <-colorRampPalette(c('yellow', 'red' ))
-Mid <- "snow3"
-MtoH <-colorRampPalette(c('darkgreen', 'lightgreen'))
-cols <- c("1" = 'darkgreen', "2" = '#2ab72e', "3" = 'lightgreen', "4" = '#f4f007', "5" = '#FECC42', "6" = '#f96a34', "7" = "red")
+cols <- c("1" = 'darkgreen', "2" = '#2ab72e', "3" = 'lightgreen', "4" = '#e3eb96', "5" = '#f4f007', "6" = '#FECC42', "7" = '#f96a34', "8" = "red")
 
 
 ## charts setup
@@ -50,9 +49,9 @@ setwd(export_directory) # setwd to exports charts to
 
 lgu_list=list()
 
-for (i in unique(lgu_input_slim$adm_code)) {
+for (i in unique(lgu_order$adm2_code)) {
 
-  lgu_filter <- filter(lgu_input_slim, adm_code == i)
+  lgu_filter <- filter(lgu_order, adm2_code == i)
 
   plot <- ggplot(lgu_filter, aes(x = trav_cat, y = pc_pop, fill = trav_value)) +
     geom_text(aes(label=paste((pc_pop*100),"%\n",cat_pop," persons",sep="")),hjust=-0.1,size=2.5) +
@@ -65,7 +64,7 @@ for (i in unique(lgu_input_slim$adm_code)) {
     scale_fill_manual(values= cols) +
     coord_flip()
 
-  filename=paste(lgu_filter$adm_name," - ",i," - ",type,".pdf",sep="")
+  filename=paste("./",export_directory,"/","Province ",lgu_filter$province," - ",lgu_filter$adm_name," - ",i," - ",type,".pdf",sep="")
 
   lgu_list[[i]] = plot
 
@@ -76,9 +75,9 @@ print("done with plots")
 
 ## LGU print loop
 
-for (i in unique(lgu_input_slim$adm_code)) { # Another for loop, this time to save out the bar charts in lgu_list as PDFs
-    lgu_filter <- filter(lgu_input_slim, adm_code == i)
-    filename=paste(lgu_filter$adm_name," - ",i," - ",type,".jpeg",sep="") # Make the file name for each PDF. The paste makes the name a variable of the admin, so each chart is named by LGU and code
+for (i in unique(lgu_order$adm2_code)) { # Another for loop, this time to save out the bar charts in lgu_list as PDFs
+    lgu_filter <- filter(lgu_order, adm2_code == i)
+    filename=paste("Province ",lgu_filter$province," - ",lgu_filter$adm_name," - ",i," - ",type,".jpeg",sep="") # Make the file name for each PDF. The paste makes the name a variable of the admin, so each chart is named by LGU and code
 
     jpeg(filename,width=3.5,height=4,units="in",bg="white",quality=1,res=300,type=c("quartz")) # jpeg basic specifications. Modify the width and height here.
 
